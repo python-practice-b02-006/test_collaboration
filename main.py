92b71aa470a4544f44b3d42c196527bb53629df4
Maxim_Povolotskii <povolotskii_mr@phystech.edu>
2020-11-13 09:59:49 +0300

    Added my function in the init

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 class OurGroup():
     def __init__(self):
+        self.povolotskii()
         pass
     
     def kolganov(self):
@@ -27,7 +28,7 @@ class OurGroup():
         pass
 
     def povolotskii_mr(self):
-        print("I am Povolotskii)
+        print("I am Povolotskii")
 
     def gribova(self):
         print("I am NG")
